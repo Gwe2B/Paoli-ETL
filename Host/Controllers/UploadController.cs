35d26a452ec744e837f2989404d9b72c672d8552
Etudiant <fahima.boudaoud@gmail.com>
2022-12-07 13:47:37 +0100

    UploadFile

---
+++ Host/Controllers/UploadController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadFile( IFormFile file )
+        {
+            
+           
+            // Save the file to the server
+            // (You can specify a different path or handle the file in a different way)
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", file.FileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok();
+        }
+    }
+}
