281a6412f0e59acf400ad062e7074de9d60387c4
Etudiant <fahima.boudaoud@gmail.com>
2022-12-07 11:23:56 +0100

    Create Split foctoid

---
+++ Services/SplitFonctoid.cs
@@ -0,0 +1,16 @@
+﻿using ServiceContract;
+using System.Reflection;
+
+namespace Services
+{
+    public class SplitFonctoid : Fonctoid
+    {
+        public Char SplitCar { get; set; }
+
+
+        public override void Proceed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
