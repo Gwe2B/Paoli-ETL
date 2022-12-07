2454e4900a9894c11c4d8e611b009917489320d1
salifou-root163 <50051019+salifou-root163@users.noreply.github.com>
2022-12-07 11:42:37 +0100

    select csv from IHM

--- IHM/Form1.Designer.cs
+++ IHM/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(437, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(86, 29);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(171, 159);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private Button button1;
+        private ListView listView1;
     }
 }
--- IHM/Form1.cs
+++ IHM/Form1.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Drawing;
+
 namespace IHM
 {
     public partial class Form1 : Form
@@ -5,6 +8,57 @@ namespace IHM
         public Form1()
         {
             InitializeComponent();
+            // Create a new ListView control.
+
+            listView1.Bounds = new Rectangle(new Point(10, 10), new Size(300, 400));
+            
+            listView1.View = View.Details;            
+            listView1.LabelEdit = true;           
+            listView1.AllowColumnReorder = true;          
+            listView1.CheckBoxes = true;            
+            listView1.FullRowSelect = true;            
+            listView1.GridLines = true;           
+            listView1.Sorting = SortOrder.Ascending;
+
+            listView1.Columns.Add("Entetes", -2, HorizontalAlignment.Left);
+            
+
+
+            ListViewItem item1 = new ListViewItem("col1", 0);
+
+            ListViewItem item2 = new ListViewItem("col2", 0);
+
+            ListViewItem item3 = new ListViewItem("col3", 0);
+            ListViewItem item4 = new ListViewItem("col4", 0);
+
+
+
+            listView1.Items.AddRange(new ListViewItem[] { item1, item2, item3, item4 });
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Access files (*.csv)|*.csv";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var path = openFileDialog1.FileName;
+            }
+
+
+        }
+
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
